5e2ea3ad4915552e10667357018e3ee98a45f020
Dan Bural <>
2022-12-27 14:40:54 +0200

    impliment ad showing functionality

--- App.js
+++ App.js
@@ -1,10 +1,96 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet, ProgressBarAndroid} from 'react-native';
+import React, {useState, useEffect, useRef} from 'react';
+import {View, Text, ProgressBarAndroid} from 'react-native';
+import {
+  InterstitialAd,
+  TestIds,
+  AdEventType,
+} from 'react-native-google-mobile-ads';
+
+const adUnitId = __DEV__
+  ? TestIds.APP_OPEN
+  : 'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy';
+
+const interstitial = InterstitialAd.createForAdRequest(adUnitId, {
+  requestNonPersonalizedAdsOnly: true,
+  keywords: ['fashion', 'clothing'],
+});
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isAdLoaded, setIsAdLoaded] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const progressInterval = useRef(null);
+  const timeout = useRef(null);
+
+  useEffect(() => {
+    const unsubscribe = interstitial.addAdEventListener(
+      AdEventType.LOADED,
+      () => {
+        setIsAdLoaded(true);
+      },
+    );
+
+    interstitial.load();
+
+    progressInterval.current = setInterval(() => {
+      setProgress(prevProgress => prevProgress + 1);
+    }, 100);
+
+    timeout.current = setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+
+    return () => {
+      unsubscribe();
+      clearInterval(progressInterval.current);
+      clearTimeout(timeout.current);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isAdLoaded) {
+      interstitial.show();
+    }
+  }, [isAdLoaded]);
+
+  useEffect(() => {
+    if (isAdLoaded || progress >= 100) {
+      clearInterval(progressInterval.current);
+    }
+  }, [isAdLoaded, progress]);
+
+  useEffect(() => {
+    const unsubscribe = interstitial.addAdEventListener(
+      AdEventType.CLOSED,
+      () => {
+        setIsLoading(false);
+      },
+    );
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!isAdLoaded && !isLoading) {
+      setIsLoading(false);
+    }
+  }, [isAdLoaded, isLoading]);
+
   return (
     <View>
-      <Text>Hello World</Text>
+      {isLoading && (
+        <>
+          <ProgressBarAndroid
+            styleAttr="Horizontal"
+            indeterminate={false}
+            progress={progress / 100}
+          />
+          <Text>Hello World</Text>
+        </>
+      )}
+      {!isLoading && <Text>Welcome to my app</Text>}
     </View>
   );
 };
